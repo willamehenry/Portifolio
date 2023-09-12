41b15ad852fc5572ad67395b935c7274432be941
willamehenry <willamehenry1996@gmail.com>
2023-09-11 22:09:00 -0300

    subindo versão 1.4

---
+++ assets/JavaScript/acordeon.js
@@ -0,0 +1,19 @@
+const acordtrigger = document.querySelectorAll('.acordeon .trigger');
+
+acordtrigger.forEach((trigger) => {
+
+    trigger.addEventListener('click', (e) => {
+        const acordeon =trigger.parentElement;
+        const isOpen = acordeon.classList.contains('open');
+
+        if (isOpen){
+
+            acordeon.classList.remove('open');
+        }else {
+
+            acordeon.classList.add('open');
+        }
+        
+    })
+
+})
